a0106d02b55c9984175a8235dc6576d44475c09e
kkardd <wlstjd4013@gmail.com>
2025-06-09 19:26:49 +0900

    의존성 에러픽스

--- src/module/refresh-token.module.ts
+++ src/module/refresh-token.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RefreshToken } from '@/domain/entity/refresh-token.entity';
 import { RefreshTokenService } from '@/infrastructure/refresh-token.service';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([RefreshToken])],
   providers: [RefreshTokenService],

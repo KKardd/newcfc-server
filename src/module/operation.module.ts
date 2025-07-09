f3622eb2aa129d593571a43c2b83716384a75871
Jinseong-Kong <kkardd@carbonsaurus.com>
2025-07-09 12:55:50 +0900

    fix: DI error

--- src/module/operation.module.ts
+++ src/module/operation.module.ts
@@ -3,17 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OperationController } from '@/adapter/inbound/controller/operation.controller';
 import { OperationRepository } from '@/adapter/outbound/operation.repository';
-import { Operation } from '@/domain/entity/operation.entity';
-import { WayPoint } from '@/domain/entity/way-point.entity';
-import { Vehicle } from '@/domain/entity/vehicle.entity';
 import { Chauffeur } from '@/domain/entity/chauffeur.entity';
 import { Garage } from '@/domain/entity/garage.entity';
+import { Operation } from '@/domain/entity/operation.entity';
 import { RealTimeDispatch } from '@/domain/entity/real-time-dispatch.entity';
 import { Reservation } from '@/domain/entity/reservation.entity';
+import { Vehicle } from '@/domain/entity/vehicle.entity';
+import { WayPoint } from '@/domain/entity/way-point.entity';
 import { OperationServiceInPort } from '@/port/inbound/operation-service.in-port';
 import { OperationServiceOutPort } from '@/port/outbound/operation-service.out-port';
 import { OperationService } from '@/port/service/operation.service';
+
 import { ChauffeurModule } from './chauffeur.module';
+import { RealTimeDispatchModule } from './real-time-dispatch.module';
 import { ReservationModule } from './reservation.module';
 import { WayPointModule } from './way-point.module';
 
@@ -23,6 +25,7 @@ import { WayPointModule } from './way-point.module';
     forwardRef(() => ChauffeurModule),
     ReservationModule,
     WayPointModule,
+    RealTimeDispatchModule,
   ],
   controllers: [OperationController],
   providers: [
